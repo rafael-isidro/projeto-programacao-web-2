664517967dcc2f76222ee7050c9ce43aebec3d99
Bruno Samuel <brunosamuel.orizona@gmail.com>
2024-04-26 19:25:32 -0300

    Projeto criado

---
+++ src/main/java/com/ada/economizaapi/EconomizaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ada.economizaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EconomizaApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EconomizaApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ada/economizaapi/EconomizaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ada.economizaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EconomizaApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
