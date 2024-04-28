1662c0df8d3bf73eeaff9886d0dbd796cdd84ab5
Bruno Samuel <brunosamuel.orizona@gmail.com>
2024-04-28 17:26:20 -0300

    Create class ListaCompra

---
+++ src/main/java/com/ada/economizaapi/entities/ListaCompra.java
@@ -0,0 +1,46 @@
+package com.ada.economizaapi.entities;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class ListaCompra {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name = "pessoa_id")
+    private Pessoa pessoa;
+    @ManyToMany
+    @JoinTable(
+            name = "lista_compra_produto",
+            joinColumns = @JoinColumn(name = "lista_compra_id"),
+            inverseJoinColumns = @JoinColumn(name = "produto_id")
+    )
+    private List<Produto> produtos = new ArrayList<>();
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ListaCompra that = (ListaCompra) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+}
