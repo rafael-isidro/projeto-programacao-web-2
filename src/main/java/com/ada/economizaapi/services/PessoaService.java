eadc25fa305f37193614b6104ad223e80a98c74b
Bruno Samuel <brunosamuel.orizona@gmail.com>
2024-04-28 16:10:44 -0300

    Finalizei os metodos de ServicoAbstrato

---
+++ src/main/java/com/ada/economizaapi/services/PessoaService.java
@@ -0,0 +1,15 @@
+package com.ada.economizaapi.services;
+
+import com.ada.economizaapi.entities.Pessoa;
+import com.ada.economizaapi.repositories.PessoaRepository;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PessoaService extends ServicoAbstrato<Pessoa, Long, PessoaRepository> {
+
+    public PessoaService(PessoaRepository repository) {
+        super(repository);
+    }
+
+}
--- src/main/java/com/ada/economizaapi/services/ServicoAbstrato.java
+++ src/main/java/com/ada/economizaapi/services/ServicoAbstrato.java
@@ -1,23 +1,41 @@
 package com.ada.economizaapi.services;
 
+import com.ada.economizaapi.exceptions.EntidadeJaExisteException;
+import com.ada.economizaapi.exceptions.EntidadeNaoExisteException;
+import jakarta.persistence.EntityManager;
+import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.support.JpaEntityInformation;
+import org.springframework.data.jpa.repository.support.JpaEntityInformationSupport;
 
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
-public abstract class ServicoAbstrato<E, R extends JpaRepository<E, Long>> {
+public abstract class ServicoAbstrato<E, ID, R extends JpaRepository<E, ID>> {
 
-    private R repository;
+    private final R repository;
 
     public ServicoAbstrato(R repository) {
         this.repository = repository;
     }
 
-    public abstract boolean exists(E entity);
+    public boolean exists(E entity) {
+        return this.repository.exists(Example.of(entity));
+    }
+
+    public boolean existsById(ID id) {
+        return this.repository.existsById(id);
+    }
 
     public E save(E entity) {
+        if (this.exists(entity)) {
+            throw new EntidadeJaExisteException();
+        }
         return this.repository.save(entity);
     }
 
@@ -25,6 +43,29 @@ public abstract class ServicoAbstrato<E, R extends JpaRepository<E, Long>> {
         return StreamSupport.stream(entities.spliterator(), false)
                 .map(this::save)
                 .collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public Optional<E> findById(ID id) {
+        return this.repository.findById(id);
+    }
+
+    public List<E> findAll() {
+        return this.repository.findAll();
+    }
+
+    public E update(E entity) {
+        if (!this.exists(entity)) {
+            throw new EntidadeNaoExisteException();
+        }
+        return this.repository.save(entity);
+    }
+
+    public void delete(E entity) {
+        this.repository.delete(entity);
+    }
+
+    public void deleteById(ID id) {
+        this.repository.deleteById(id);
     }
 
 }
