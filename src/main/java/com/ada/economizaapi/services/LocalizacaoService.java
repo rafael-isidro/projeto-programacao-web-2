1cef886cdc111fe2584e2bc7241c3bb38e0865e3
Bruno Samuel <brunosamuel.orizona@gmail.com>
2024-04-28 21:15:33 -0300

    Ignorando certificado SSL

--- src/main/java/com/ada/economizaapi/services/LocalizacaoService.java
+++ src/main/java/com/ada/economizaapi/services/LocalizacaoService.java
@@ -5,23 +5,40 @@ import com.ada.economizaapi.services.responses.DistanceResponse;
 import com.ada.economizaapi.services.responses.Feature;
 import com.ada.economizaapi.services.responses.Properties;
 import com.ada.economizaapi.services.responses.Segment;
-import org.springframework.stereotype.Service;
+import io.netty.handler.ssl.SslContext;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
-@Service
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import reactor.netty.http.client.HttpClient;
+import io.netty.handler.ssl.SslContextBuilder;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import javax.net.ssl.SSLException;
+
 public class LocalizacaoService {
+
     private final String API_KEY = "5b3ce3597851110001cf624822b8f2ef32ca4aa0906695f5d6a8e19c";
 
     public Double retornarDistanciaKm(Localizacao localizacaoOrigem, Localizacao localizacaoDestino) {
+        SslContext sslContext;
+        try {
+            sslContext = SslContextBuilder.forClient()
+                    .trustManager(InsecureTrustManagerFactory.INSTANCE)
+                    .build();
+        } catch (SSLException e) {
+            throw new RuntimeException(e);
+        }
 
-        return WebClient
-                .create()
+        HttpClient httpClient = HttpClient.create().secure(sslContextSpec -> sslContextSpec.sslContext(sslContext));
+        ReactorClientHttpConnector connector = new ReactorClientHttpConnector(httpClient);
+
+        return WebClient.builder().clientConnector(connector)
+                .baseUrl("https://api.openrouteservice.org/v2")
+                .build()
                 .get()
                 .uri(uriBuilder -> uriBuilder
-                        .scheme("https")
-                        .host("api.openrouteservice.org")
-                        .path("/v2/directions/driving-car")
+                        .path("/directions/driving-car")
                         .queryParam("api_key", API_KEY)
                         .queryParam("start", localizacaoOrigem.getCoordenadas())
                         .queryParam("end", localizacaoDestino.getCoordenadas())
